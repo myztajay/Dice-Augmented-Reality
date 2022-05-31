a4da36177e32767e5c307b1d39e32807f48f7e67
Rafael Fernandez <myztajay@hotmail.com>
2022-05-31 16:30:53 -0400

    added plane detection and feedback to the user if they touched the plane

--- dicey-AR/ViewController.swift
+++ dicey-AR/ViewController.swift
@@ -87,6 +87,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            if !results.isEmpty{
+                print("touched plane")
+            } else {
+                print("TOUCHED SOMEWHERE ELSE")
+            }
+        }
+    }
 
     // MARK: - ARSCNViewDelegate
     
